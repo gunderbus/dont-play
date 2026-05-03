f3e26bc219e8c8c56f2a6dd1b7b71d8af1e0a5b3
gunderbus <cole.gundermann@gmail.com>
2026-05-03 14:13:13 -0400

    i hate bayeko

--- include/level/level.cpp
+++ include/level/level.cpp
@@ -11,3 +11,33 @@ void Level::render() {
     // For now, print a placeholder message.
     std::cout << "Level::render() not implemented yet" << std::endl;
 }
+
+void Level::testPrint() {
+    // TODO: implement test print logic.
+    // Print level information for debugging purposes.
+}
+
+void Level::setTile(int x, int y, std::string tileType) {
+    // TODO: implement tile setting logic.
+    // Set the tile at position (x, y) to the specified type.
+}
+
+void Level::addTileType(std::string tileName, block tileBlock) {
+    // TODO: implement tile type addition logic.
+    // Add a new tile type with the specified name and properties.
+}
+
+void Level::removeTileType(std::string tileName) {
+    // TODO: implement tile type removal logic.
+    // Remove the tile type with the specified name.
+}
+
+std::string Level::getTileInfo(int x, int y) {
+    // TODO: implement tile info retrieval logic.
+    // Return information about the tile at position (x, y).
+}
+
+block Level::getTileTypeInfo(std::string tileType) {
+    // TODO: implement tile type info retrieval logic.
+    // Return the properties of the specified tile type.
+}
